7eb28377574175dc2f00fe0a4c87cf6ffdf31df9
Vicente <vicente.orlandini@gmail.com>
2025-10-06 12:02:30 -0300

    Control Flota v1.2
    Se agrega BD y Tablas en los models

--- AppFlota/models.py
+++ AppFlota/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Chofer(models.Model):
+    Rut = models.CharField(max_length=10)
+    Nombre = models.CharField(max_length=50)
+    Fecha_Nacimiento = models.CharField(max_length=10)
+    Telefono = models.CharField(max_length=10)
+    Estado = models.BooleanField(default=True)
+    Horas = models.CharField(max_length=30)
+    Email = models.EmailField()
+
+class Combustible(models.Model):
+    Tipo_Combustible = models.CharField(max_length=45)
+    Fecha_Recarga = models.CharField(max_length=10)
+    Lugar = models.CharField(max_length=55)
+    Encargado = models.CharField(max_length=40)
+    Cantidad_Estanque = models.CharField(max_length=40)
+    Recargar = models.CharField(max_length=40)
+
+class Mantencion(models.Model):
+    Tipo_Mantencion = models.CharField(max_length=50)
+    Fecha_Mantencion = models.CharField(max_length=20)
+    Lugar = models.CharField(max_length=60)
+    Detalle = models.CharField(max_length=100)
--- ControlFlota/settings.py
+++ ControlFlota/settings.py
@@ -1,3 +1,5 @@
+
+
 """
 Django settings for ControlFlota project.
 
@@ -12,7 +14,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,8 +81,10 @@ WSGI_APPLICATION = 'ControlFlota.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'controlflota',
+        'USER' : 'root',
+        'PASSWORD' : '123456'
     }
 }
 
