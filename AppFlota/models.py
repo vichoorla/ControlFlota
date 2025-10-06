1c6538ce35eb54d5f52ab91e805b15719efa2047
bylay <byly699@gmail.com>
2025-10-06 12:05:30 -0300

    Modelo de vehiculo
    modelo vehiculo

--- AppFlota/models.py
+++ AppFlota/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Vehiculo(models.Model):
+   patente = models.CharField(max_length=6)
+   VIN = models.CharField(max_length=17)
+   marca = models.CharField(max_length=50)
+   modelo = models.CharField(max_length=50)
+   año = models.CharField(max_length=4)
+   motor = models.CharField(max_length=50)
+   seguro = models.BooleanField(default=True)
+   revision_tecnica = models.BooleanField(default=True)
+   permiso_circulacion = models.BooleanField(default=True)
+   gps = models.BooleanField(default=True)
+   kilometraje = models.PositiveBigIntegerField(default=0)
+   estanque = models.DecimalField(max_digits=5, decimal_places=2,
+    help_text="Capacidad en litros")
+   tonelaje = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+   asignacion = models.CharField(max_length=50)
